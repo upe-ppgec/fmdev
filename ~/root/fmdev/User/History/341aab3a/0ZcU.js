ea4fd76757d234bf7125a8c93f06b09b22f4ea13
João Ferreira <joaoferreirape@users.noreply.github.com>
2023-08-21 23:01:26 -0300

    Corrigindo comit
    Foi enviada a pasta ~/root por engano

--- ~/root/fmdev/User/History/341aab3a/0ZcU.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const Content = styled.div`
-  display: flex;
-  padding-top: 0;
-  margin: 2rem;
-`;
--- ~/root/fmdev/User/History/341aab3a/QFBC.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const h2 = styled.div`
-  display: flex;
-  margin-left: 30px;
-`;
